57871076a2cccdc25ca38027657f5afcfb041fbc
Chat Wacharamanotham <chat@ifi.uzh.ch>
2026-04-21 21:20:34 +0200

    Change code to handle one scan file per exam version

--- R/03_process_scanned_sheets.R
+++ R/03_process_scanned_sheets.R
@@ -4,13 +4,13 @@ library("exams")
 
 
 root_path <- "workspace/"
-exam_start_id <- "10003"
+exam_start_id <- "All"
 
 register_path <- path(root_path, "04 Student info.csv")
 meta_data_path <- path(root_path, sprintf("03 Generated exam answersheet/%s/metainfo.rds", exam_start_id))
 processed_zip_path <- path(root_path, sprintf("06 Processed zip/%s", exam_start_id))
 eval_output_path <- path(root_path, sprintf("07 Evaluation output/%s", exam_start_id))
-exam_scan_path <- path(root_path, sprintf("05 Exam scan - %s.pdf", exam_start_id))
+exam_scan_path <- path(root_path, sprintf("05 Exam scan/Scan%s.pdf", exam_start_id))
 
 #-------------------------------------------------------------------------------
 dir_create(processed_zip_path, recurse = TRUE)
@@ -23,17 +23,21 @@ scan_results <-
             verbose = TRUE,
             dir = processed_zip_path)
 
-# check and manually fix problems
-# nops_fix(
-  # Sys.glob(path(root_path, "04 Processed zip/nops_scan_*.zip")))
+# output zip path
+output_zip_path <- 
+  Sys.glob(
+    path(root_path, "06 Processed zip", exam_start_id, "nops_scan_*.zip")
+  )
 
+# check and manually fix problems
+nops_fix(scans = output_zip_path)
 
 #-------------------------------------------------------------------------------
 # evaluation
 ev1 <- nops_eval(
   register = register_path,
   solutions = meta_data_path,
-  scans = Sys.glob(path(processed_zip_path, "nops_scan_*.zip")),
+  scans = output_zip_path,
   eval = exams_eval(partial = FALSE, negative = FALSE),
   dir = eval_output_path
 )
