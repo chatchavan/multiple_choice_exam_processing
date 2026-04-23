a33ac39cb5d624b201539e660262a70ae9e8d7d5
Chat Wacharamanotham <chat@ifi.uzh.ch>
2026-04-23 19:47:24 +0200

    - Combine RDS, so that we don't need to sort the scans manually. - Auto-transform CSV student register file to format that r-exam wants

--- R/03_process_scanned_sheets.R
+++ R/03_process_scanned_sheets.R
@@ -2,17 +2,19 @@ library(tidyverse)
 library(fs)
 library("exams")
 
-
+student_id_length <- 8 # UZH: 12-345-678
 root_path <- "workspace/"
-exam_start_id <- "All"
 
+meta_data_path <- path(root_path,"03.1-combinedRDS.rds")
 register_path <- path(root_path, "04 Student info.csv")
-meta_data_path <- path(root_path, sprintf("03 Generated exam answersheet/%s/metainfo.rds", exam_start_id))
-processed_zip_path <- path(root_path, sprintf("06 Processed zip/%s", exam_start_id))
-eval_output_path <- path(root_path, sprintf("07 Evaluation output/%s", exam_start_id))
-exam_scan_path <- path(root_path, sprintf("05 Exam scan/Scan%s.pdf", exam_start_id))
+exam_scan_path <- path(root_path, "05 Exam scan.pdf")
+processed_zip_path <- path(root_path, "06 Processed zip/")
+eval_output_path <- path(root_path, "07 Evaluation output/")
+
 
 #-------------------------------------------------------------------------------
+register_clean_path <- path(root_path, "04.1 Student info clean.csv")
+
 dir_create(processed_zip_path, recurse = TRUE)
 dir_create(eval_output_path, recurse = TRUE)
 #-------------------------------------------------------------------------------
@@ -26,16 +28,36 @@ scan_results <-
 # output zip path
 output_zip_path <- 
   Sys.glob(
-    path(root_path, "06 Processed zip", exam_start_id, "nops_scan_*.zip")
+    path(root_path, "06 Processed zip", "nops_scan_*.zip")
   )
 
 # check and manually fix problems
 nops_fix(scans = output_zip_path)
 
+
+#-------------------------------------------------------------------------------
+# prep register into the right form
+
+reg_df <- read_delim(register_path, col_types = cols(.default = col_character()))
+assertthat::has_name(reg_df, "registration")
+assertthat::has_name(reg_df, "name")
+assertthat::has_name(reg_df, "id")
+
+# remove dashes in the ID
+reg_df <- 
+  reg_df %>% 
+  mutate(registration = str_replace_all(registration, "-", ""))   
+
+# ensure student id length
+assertthat::see_if(all(str_length(reg_df$registration) == student_id_length))
+
+# use semi-colon
+write_delim(reg_df, register_clean_path, delim = ";")
+
 #-------------------------------------------------------------------------------
 # evaluation
 ev1 <- nops_eval(
-  register = register_path,
+  register = register_clean_path,
   solutions = meta_data_path,
   scans = output_zip_path,
   eval = exams_eval(partial = FALSE, negative = FALSE),
